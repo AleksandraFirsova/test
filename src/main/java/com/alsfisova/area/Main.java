58a9fa1591bf6cfdf4f33e38627afe9b2b418e17
AleksandraFirsova <63194221+AleksandraFirsova@users.noreply.github.com>
2020-04-07 17:32:40 +0300

    changes

--- src/main/java/com/alsfisova/area/Main.java
+++ src/main/java/com/alsfisova/area/Main.java
@@ -2,7 +2,7 @@ package com.alsfisova.area;
 
 public class Main {
     public static void main(String[] args) {
-        Triangle triangle = new Triangle(2, 4, 6);
+        Triangle triangle = new Triangle(2, 4, 5);
         Square square = new Square(4);
         Circle circle = new Circle(2.0);
 
